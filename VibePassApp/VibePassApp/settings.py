2758bc7f135170157f4c0d0ae0f52edc4a8519cf
Mark Macharia <markmacharia124@gmail.com>
2026-09-12 14:55:17 +0300

    fixing got an unexpected keyword argument 'ssl_cert_reqs' error

--- VibePassApp/VibePassApp/settings.py
+++ VibePassApp/VibePassApp/settings.py
@@ -190,7 +190,6 @@ CHANNEL_LAYERS = {
             "hosts": [
                 {
                     "host": os.getenv("CELERY_BROKER_URL"),
-                    "ssl_cert_reqs": None, # Disable SSL certificate verification
                     "port": 6379,
                     # Seconds to wait for a socket read before raising TimeoutError
                     "socket_timeout": 60,
